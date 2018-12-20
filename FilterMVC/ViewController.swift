367bd1f3664a20a4c255497ee06d3398e859c375
Alfian Losari <alfianlosari@gmail.com>
2018-12-21 05:18:51 +0700

    Initial Commit

---
+++ FilterMVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FilterMVC
+//
+//  Created by Alfian Losari on 12/21/18.
+//  Copyright © 2018 Alfian Losari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
